f9afc9bd69ac15ff33423ad68b19107c724f8112
Tammy Richardson <atrich0220@gmail.com>
2020-02-16 14:12:19 -0700

    fixed keyboard so letters would populate and fixed guessed word so dashes would populate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,89 +20,56 @@ var movieNames = [
   "magnolia"
 ];
 
+
 var answer = "";
-var maxWrong = 10;
+var maxWrong = 15;
 var mistakes = 0;
 var guessed = [];
+var wordStatus = null;
 
 //this will pick a random title to start
 function randomTitle() {
   answer = movieNames[Math.floor(Math.random() * movieNames.length)];
-  document.getElementById("main-heading");
 }
 
+//create keyboard buttons and link letters to buttons
 function generateButtons() {
-  var buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
-    .split("")
-    .map(
-      (letter) =>
-        `
-    <button
-    class="btn btn-sm btn-danger m-1"
-    id="` +
-        letter +
-        `
-    onClick="handleGuess("` +
-        letter +
-        `" )"
-    >
-      ` +
-        letter +
-        `
-      </button>
-  `
-    )
-    .join(" ");
-
+  var buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split("").map((letter) =>
+    '<button class="btn btn-sm btn-danger m-1" id="' + letter + '" onClick="console.log(\'' + letter +
+    '\');handleGuess(\'' + letter + '\')">'
+    + letter + '</button>').join(" ");
   document.getElementById("keyboard").innerHTML = buttonsHTML;
 }
 
+//disable buttons after clicked so the cannot be clicked again
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute("disabled", true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+  }
+}
+//check if the letter exist in the title
+function guessedWord() {
+  wordStatus = answer.split("").map((letter) => (guessed.indexOf(letter) >= 0 ? letter : " _ "))
+    .join("");
+  document.getElementById("wordSpotlight").innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById("mistakes").innerHTML = mistakes;
+}
+
+document.getElementById("maxWrong").innerHTML = maxWrong;
+
+
 randomTitle();
 generateButtons();
+guessedWord();
 
-//sets up answerArray to show how many letters are in the title
-//using under scores
-var answerArray = [];
-for (var i = 0; i < movieNames.length; i++) {
-  answerArray[i] = "_";
-}
 
-// variable to show the number of remaining letters to be guessed
-var remainingLetters = movieNames.length;
 
-//    ***** The Main Game Loop *****
-
-//while there are letters left to be guessed
-while (remainingLetters > 0) {
-  //show player their progress
-  alert(answerArray.join(" "));
-
-  //get a guess from the player
-  var guess = prompt("Guess a letter or click cancel to quit!");
-
-  //if the guess is blank
-  if (guess === null) {
-    //exit the game loop
-    break;
-    //if the guess is more than one letter or no letters
-  } else if (guess.length !== 1) {
-    //alert them to guess only a single letter
-    alert("Please enter a single letter!");
-    break;
-    //if they have a valid guess
-  } else {
-    //update the game state with the guess
-    for (var j = 0; j < movieNames.length; j++) {
-      //if the letter guessed is in the title at that point or index
-      if (movieNames[j].includes(guess)) {
-        console.log;
-      }
-    }
-  }
-  //     ***** end of main game loop ******
-}
-
-//let player know the word
-alert(answerArray.join(" "));
-//congratulate the player
-alert("Good job! The answer was " + movieNames);
