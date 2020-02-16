0596bcb9dd9b0a1e911065c9f235cd1cc8827632
Tammy Richardson <atrich0220@gmail.com>
2020-02-16 10:54:04 -0700

    added keyboard and onclick let's play h1 element

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,74 @@
 //array of movie titles
 var movieNames = [
-  "pulp fiction",
   "matrix",
   "titanic",
-  "jurassic park",
   "scream",
-  "empire records",
-  "last action hero",
-  "12 monkeys",
-  "the sandlot",
   "jumanji",
-  "edward scissorhands",
-  "forrest gump",
-  "the sixth sense",
   "fargo",
-  "saving private ryan",
-  "the usual suspects",
-  "reservoir dogs",
-  "trainspotting"
+  "trainspotting",
+  "goodfellas",
+  "misery",
+  "awakenings",
+  "aladdin",
+  "unforgiven",
+  "philadelphia",
+  "clerks",
+  "braveheart",
+  "seven",
+  "casino",
+  "gattaca",
+  "magnolia"
 ];
-var startButton = document.getElememtById("startButton");
+
+var answer = "";
+var maxWrong = 10;
+var mistakes = 0;
+var guessed = [];
+
 //this will pick a random title to start
-var title = movieNames[Math.floor(Math.random() * movieNames.length)];
+function randomTitle() {
+  answer = movieNames[Math.floor(Math.random() * movieNames.length)];
+  document.getElementById("main-heading");
+}
+
+function generateButtons() {
+  var buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+        `
+    <button
+    class="btn btn-sm btn-danger m-1"
+    id="` +
+        letter +
+        `
+    onClick="handleGuess("` +
+        letter +
+        `" )"
+    >
+      ` +
+        letter +
+        `
+      </button>
+  `
+    )
+    .join(" ");
+
+  document.getElementById("keyboard").innerHTML = buttonsHTML;
+}
+
+randomTitle();
+generateButtons();
 
 //sets up answerArray to show how many letters are in the title
 //using under scores
 var answerArray = [];
-for (var i = 0; i < movieName.length; i++) {
+for (var i = 0; i < movieNames.length; i++) {
   answerArray[i] = "_";
 }
 
 // variable to show the number of remaining letters to be guessed
-var remainingLetters = movieName.length;
+var remainingLetters = movieNames.length;
 
 //    ***** The Main Game Loop *****
 
@@ -44,23 +81,21 @@ while (remainingLetters > 0) {
   var guess = prompt("Guess a letter or click cancel to quit!");
 
   //if the guess is blank
-  if (guess == null) {
+  if (guess === null) {
     //exit the game loop
     break;
     //if the guess is more than one letter or no letters
-  } else if (guess.lenght !== 1) {
+  } else if (guess.length !== 1) {
     //alert them to guess only a single letter
     alert("Please enter a single letter!");
+    break;
     //if they have a valid guess
   } else {
     //update the game state with the guess
-    for (var j = 0; j < movieName.length; j++) {
+    for (var j = 0; j < movieNames.length; j++) {
       //if the letter guessed is in the title at that point or index
-      if (movieName[j] === guess) {
-        //update the answerArray with the letter guessed
-        answerArray[j] = guess;
-        //subtract one from the remaining letters
-        remainingLetters--;
+      if (movieNames[j].includes(guess)) {
+        console.log;
       }
     }
   }
@@ -70,4 +105,4 @@ while (remainingLetters > 0) {
 //let player know the word
 alert(answerArray.join(" "));
 //congratulate the player
-alert("Good job! The answer was " + movieName);
+alert("Good job! The answer was " + movieNames);
